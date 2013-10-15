aa5f9a54ee9677772a76fdb1c26798cd4b20fe5c
Christopher Smith <chris@jalakai.co.uk>
2013-10-15 18:33:52 +0100

    Rework tests making the code slightly simpler & replacing '\n' with NL constant.

--- _test/syntax.test.php
+++ _test/syntax.test.php
@@ -5,224 +5,224 @@
 class plugin_definitionlist_syntax_test extends DokuWikiTest {
 
     protected $pluginsEnabled = array('definitionlist');
+    protected $renderer = null;
 
-    function test_basic() {
-        $in1 = "\n"
-              ."  ; Term\n"
-              ."  :Definition\n";
-        $in2 = "\n  ; Term : Definition\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd>Definition</dd>\n"
-            ."</dl>\n";
+    function setUp() {
+        parent::setUp();
 
-        $renderer = new Doku_Renderer_xhtml();
-        $actual1 = $renderer->render($in1, 'xhtml');
-        $this->assertEquals($expected, $actual1);
-        $actual2 = $renderer->render($in2, 'xhtml');
-        $this->assertEquals($expected, $actual2);
+        $this->renderer = new Doku_Renderer_xhtml();
+    }
+
+    protected function render($rawwiki,$format='xhtml') {
+        return $this->renderer->render($rawwiki,$format);
+    }
+
+    function test_basic_singleline() {
+        $in = NL.'  ; Term : Definition'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd>Definition</dd>'.NL
+                   .'</dl>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
+    }
+
+    function test_basic_multiline() {
+        $in = NL.'  ; Term'.NL
+                .'  : Definition'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Definition</dd>'.NL
+                   .'</dl>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_multiple_definitions() {
-        $in = "\n"
-              ."  ; Term\n"
-              ."  : Definition one\n"
-              ."  : Definition two\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Definition one</dd>\n"
-            ."<dd> Definition two</dd>\n"
-            ."</dl>\n";
+        $in = NL.'  ; Term'.NL
+                .'  : Definition one'.NL
+                .'  : Definition two'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Definition one</dd>'.NL
+                   .'<dd> Definition two</dd>'.NL
+                   .'</dl>'.NL;
 
-        $renderer = new Doku_Renderer_xhtml();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_newline_in_definition() {
-        $in = "\n"
-              ."  ; Term\n"
-              ."  : Definition one\n"
-              ."continues\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Definition one\ncontinues</dd>\n"
-            ."</dl>\n";
+        $in = NL.'  ; Term'.NL
+                .'  : Definition one'.NL
+                .'continues'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Definition one'.NL
+                   .'continues</dd>'.NL
+                   .'</dl>'.NL;
 
-        $renderer = new Doku_Renderer_xhtml();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_newline_in_definition_with_following_para() {
-        $in = "\n"
-            ."  ; Term\n"
-            ."  : Definition one\n"
-            ."continues\n"
-            ."\n"
-            ."Then new paragraph.\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Definition one\ncontinues</dd>\n"
-            ."</dl>\n"
-            ."\n"
-            ."<p>\nThen new paragraph.\n</p>\n";
-        $renderer = new Doku_Renderer_xhtml();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $in = NL.'  ; Term'.NL
+                .'  : Definition one'.NL
+                .'continues'.NL
+                .NL
+                .'Then new paragraph.'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Definition one'.NL
+                   .'continues</dd>'.NL
+                   .'</dl>'.NL
+                   .NL
+                   .'<p>'.NL
+                   .'Then new paragraph.'.NL
+                   .'</p>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_basic_with_following_preformatted() {
-        $in = "\n"
-            ."  ; Term\n"
-            ."  : Definition\n"
-            ."\n"
-            ."  Preformatted\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Definition</dd>\n"
-            ."</dl>\n"
-            ."<pre class=\"code\">Preformatted</pre>\n";
-        $renderer = new Doku_Renderer_xhtml();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $in = NL.'  ; Term'.NL
+                .'  : Definition'.NL
+                .NL
+                .'  Preformatted'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Definition</dd>'.NL
+                   .'</dl>'.NL
+                   .'<pre class="code">Preformatted</pre>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_nonfancy() {
         global $conf;
-        $in1 = "\n"
-              ."  ; Term\n"
-              ."  :Definition\n";
-        $in2 = "\n  ; Term : Definition\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt> Term</dt>\n"
-            ."<dd>Definition</dd>\n"
-            ."</dl>\n";
-
         $conf['plugin']['definitionlist']['dt_fancy'] = false;
-        $renderer = new Doku_Renderer_xhtml();
-        $actual1 = $renderer->render($in1, 'xhtml');
-        $this->assertEquals($expected, $actual1);
-        $actual2 = $renderer->render($in2, 'xhtml');
-        $this->assertEquals($expected, $actual2);
-        unset($conf['plugin']['definitionlist']);
+
+        $in1 = NL.'  ; Term'.NL
+                 .'  : Definition'.NL;
+        $in2 = NL.'  ; Term : Definition'.NL;
+        $expected1 = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt> Term</dt>'.NL
+                   .'<dd> Definition</dd>'.NL
+                   .'</dl>'.NL;
+        $expected2 = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt> Term</dt>'.NL
+                   .'<dd>Definition</dd>'.NL
+                   .'</dl>'.NL;
+
+
+        $this->assertEquals($expected1, $this->render($in1));
+        $this->assertEquals($expected2, $this->render($in2));
     }
 
     function test_custom_class_name() {
         global $conf;
-        $in = "\n"
-              ."  ; Term\n"
-              ."  : Definition\n";
-        $expected = "<dl class=\"lorem-ipsum\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Definition</dd>\n"
-            ."</dl>\n";
+        $conf['plugin']['definitionlist']['classname'] = 'foo';
 
-        $conf['plugin']['definitionlist']['classname'] = 'lorem-ipsum';
-        $renderer = new Doku_Renderer_xhtml();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
-        unset($conf['plugin']['definitionlist']);
+        $in = NL.'  ; Term'.NL
+                .'  : Definition'.NL;
+        $expected = '<dl class="foo">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Definition</dd>'.NL
+                   .'</dl>'.NL;
+
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_two_dlists_with_blank_line_between() {
-        $in = "\n"
-              ."  ; Term : Def\n"
-              ."\n"
-              ."  ; Another term : Def\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd>Def</dd>\n"
-            ."</dl>\n"
-            ."<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Another term</span></dt>\n"
-            ."<dd>Def</dd>\n"
-            ."</dl>\n";
-        $renderer = new Doku_Renderer();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $in = NL.'  ; Term : Definition'.NL
+             .NL.'  ; Another term : Def'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd>Definition</dd>'.NL
+                   .'</dl>'.NL
+                   .'<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Another term</span></dt>'.NL
+                   .'<dd>Def</dd>'.NL
+                   .'</dl>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_dd_with_ul() {
-        $in = "\n"
-              ."  ; Term\n"
-              ."  : Some parts:\n"
-              ."  * Part 1\n"
-              ."  * Part 2\n"
-              ."\n"
-              ."  ; Term 2\n"
-              ."  : Def\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Some parts:<ul>\n"
-            ."<li class=\"level1\"><div class=\"li\"> Part 1</div>\n"
-            ."</li>\n"
-            ."<li class=\"level1\"><div class=\"li\"> Part 2</div>\n"
-            ."</li>\n"
-            ."</ul>\n"
-            ."</dd>\n"
-            ."<dt><span class=\"term\"> Term 2</span></dt>\n"
-            ."<dd> Def</dd>\n"
-            ."</dl>\n";
-        $renderer = new Doku_Renderer();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $in = NL.'  ; Term'.NL
+                .'  : Some parts:'.NL
+                .'  * Part 1'.NL
+                .'  * Part 2'.NL
+                .NL
+                .'  ; Term 2'.NL
+                .'  : Def'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Some parts:<ul>'.NL
+                   .'<li class="level1"><div class="li"> Part 1</div>'.NL
+                   .'</li>'.NL
+                   .'<li class="level1"><div class="li"> Part 2</div>'.NL
+                   .'</li>'.NL
+                   .'</ul>'.NL
+                   .'</dd>'.NL
+                   .'<dt><span class="term"> Term 2</span></dt>'.NL
+                   .'<dd> Def</dd>'.NL
+                   .'</dl>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_dd_with_ul_followed_by_ordered_list() {
-        $in = "\n"
-              ."  ; Term\n"
-              ."  : Some parts:\n"
-              ."  * Part 1\n"
-              ."  * Part 2\n"
-              ."\n"
-              ."\n"
-              ."  - Item\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Some parts:<ul>\n"
-            ."<li class=\"level1\"><div class=\"li\"> Part 1</div>\n"
-            ."</li>\n"
-            ."<li class=\"level1\"><div class=\"li\"> Part 2</div>\n"
-            ."</li>\n"
-            ."</ul>\n"
-            ."</dd>\n"
-            ."</dl>\n"
-            ."<ol>\n"
-            ."<li class=\"level1\"><div class=\"li\"> Item</div>"
-            ."\n</li>\n"
-            ."</ol>\n";
-        $renderer = new Doku_Renderer();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $in = NL.'  ; Term'.NL
+                .'  : Some parts:'.NL
+                .'  * Part 1'.NL
+                .'  * Part 2'.NL
+                .NL
+                .NL
+                .'  - Item'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Some parts:<ul>'.NL
+                   .'<li class="level1"><div class="li"> Part 1</div>'.NL
+                   .'</li>'.NL
+                   .'<li class="level1"><div class="li"> Part 2</div>'.NL
+                   .'</li>'.NL
+                   .'</ul>'.NL
+                   .'</dd>'.NL
+                   .'</dl>'.NL
+                   .'<ol>'.NL
+                   .'<li class="level1"><div class="li"> Item</div>'.NL
+                   .'</li>'.NL
+                   .'</ol>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
     function test_dd_with_ul_followed_by_2nd_dl() {
-        $in = "\n"
-              ."  ; Term\n"
-              ."  : Some parts:\n"
-              ."  * Part 1\n"
-              ."  * Part 2\n"
-              ."\n"
-              ."\n"
-              ."  ; Another term : Definition\n";
-        $expected = "<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Term</span></dt>\n"
-            ."<dd> Some parts:<ul>\n"
-            ."<li class=\"level1\"><div class=\"li\"> Part 1</div>\n"
-            ."</li>\n"
-            ."<li class=\"level1\"><div class=\"li\"> Part 2</div>\n"
-            ."</li>\n"
-            ."</ul>\n"
-            ."</dd>\n"
-            ."</dl>\n"
-            ."<dl class=\"plugin_definitionlist\">\n"
-            ."<dt><span class=\"term\"> Another term</span></dt>\n"
-            ."<dd>Definition</dd>\n"
-            ."</dl>\n";
-        $renderer = new Doku_Renderer();
-        $actual = $renderer->render($in, 'xhtml');
-        $this->assertEquals($expected, $actual);
+        $in = NL.'  ; Term'.NL
+                .'  : Some parts:'.NL
+                .'  * Part 1'.NL
+                .'  * Part 2'.NL
+                .NL
+                .NL
+                .'  ; Term 2'.NL
+                .'  : Def'.NL;
+        $expected = '<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term</span></dt>'.NL
+                   .'<dd> Some parts:<ul>'.NL
+                   .'<li class="level1"><div class="li"> Part 1</div>'.NL
+                   .'</li>'.NL
+                   .'<li class="level1"><div class="li"> Part 2</div>'.NL
+                   .'</li>'.NL
+                   .'</ul>'.NL
+                   .'</dd>'.NL
+                   .'</dl>'.NL
+                   .'<dl class="plugin_definitionlist">'.NL
+                   .'<dt><span class="term"> Term 2</span></dt>'.NL
+                   .'<dd> Def</dd>'.NL
+                   .'</dl>'.NL;
+
+        $this->assertEquals($expected, $this->render($in));
     }
 
 }
